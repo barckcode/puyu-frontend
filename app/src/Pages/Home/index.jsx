337caaed3445e05979269281d9225cf494699de3
ccordova <cristian@helmcode.com>
2024-05-02 16:18:09 +0100

    Added Navbar

--- app/src/Pages/Home/index.jsx
+++ app/src/Pages/Home/index.jsx
@@ -1,5 +1,10 @@
+import Navbar from "../../Components/Navbar"
+
 export default function Home() {
     return (
-        <h1>Home</h1>
+        <>
+            <Navbar />
+            <h1>Home</h1>
+        </>
     )
 }
