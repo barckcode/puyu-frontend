90d1522d77414e7cb6475dc7b9c9a608e395e62b
ccordova <cristian@helmcode.com>
2024-05-02 15:44:18 +0100

    Added React Router && Basic Pages

---
+++ app/src/Pages/App/index.jsx
@@ -0,0 +1,25 @@
+import { useRoutes, BrowserRouter } from "react-router-dom"
+import Home from '../Home'
+import Login from '../Login'
+import NotFound from '../NotFound'
+import './App.css'
+
+
+const AppRoutes = () => {
+	let routes = useRoutes([
+		{ path: '/', element: <Home /> },
+		{ path: '/login', element: <Login /> },
+		{ path: '/*', element: <NotFound /> },
+	])
+
+	return routes
+}
+
+
+export default function App() {
+	return (
+		<BrowserRouter>
+			<AppRoutes />
+		</BrowserRouter>
+	)
+}
