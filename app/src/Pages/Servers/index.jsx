33a0331acfa05e3a37f8b0304ad4a94f5e3269f4
barckcode <cristian@helmcode.com>
2024-06-05 21:13:59 +0100

    Added Form to Servers Page

--- app/src/Pages/Servers/index.jsx
+++ app/src/Pages/Servers/index.jsx
@@ -6,7 +6,10 @@ import { SiOvh } from 'react-icons/si';
 
 export default function CloudServices({ backendUrl, session }) {
     const [cloudServices, setCloudServices] = useState([]);
-    const [selectedService, setSelectedService] = useState(null);
+    const [selectedCloud, setSelectedCloud] = useState(null);
+    const [regions, setRegions] = useState([]);
+    const [selectedRegion, setSelectedRegion] = useState(null);
+    const [distributions, setDistributions] = useState([]);
 
     useEffect(() => {
         const fetchCloudServices = async () => {
@@ -26,41 +29,87 @@ export default function CloudServices({ backendUrl, session }) {
         fetchCloudServices();
     }, [backendUrl, session]);
 
-    const getIcon = (name) => {
-        switch (name) {
-            case 'AWS':
-                return <FaAws />;
-            case 'OVH':
-                return <SiOvh />;
-            default:
-                return null; // default icon
-        }
-    };
+    useEffect(() => {
+        if (selectedCloud) {
+            const fetchRegions = async () => {
+                try {
+                    const response = await axios.get(`${backendUrl}/cloud/${selectedCloud.id}/regions`, {
+                        headers: {
+                            Authorization: `Bearer ${session.access_token}`
+                        }
+                    });
+                    setRegions(response.data);
+                    const europeRegion = response.data.find(region => region.name.includes("Europe"));
+                    setSelectedRegion(europeRegion ? europeRegion.id : response.data[0]?.id);
+                } catch (error) {
+                    console.error('Error fetching regions:', error);
+                    setRegions([]);
+                }
+            };
 
-    const handleServiceClick = (name) => {
-        setSelectedService(name);
-    };
+            fetchRegions();
+        }
+    }, [selectedCloud, backendUrl, session]);
+
+    useEffect(() => {
+        if (selectedRegion) {
+            const fetchDistributions = async () => {
+                try {
+                    const response = await axios.get(`${backendUrl}/aws/region/${selectedRegion}/amis`, {
+                        headers: {
+                            Authorization: `Bearer ${session.access_token}`
+                        }
+                    });
+                    setDistributions(response.data);
+                } catch (error) {
+                    console.error('Error fetching distributions:', error);
+                    setDistributions([]);
+                }
+            };
+
+            fetchDistributions();
+        }
+    }, [selectedRegion, backendUrl, session]);
 
     return (
         <>
-            <h2 className="text-center p-8 text-2xl font-bold"> ☁️ Cloud Provider</h2>
+            <h2 className="text-center p-8 text-2xl font-bold"> ☁️ Cloud Providers</h2>
             <div className="grid grid-cols-2 gap-4">
                 {cloudServices.map(service => (
                     <div
                         key={service.id}
-                        className={`p-4 border rounded-lg shadow cursor-pointer hover:bg-indigo-100 hover:text-indigo-900 ${selectedService === service.name ? 'border-4 border-indigo-500' : 'border'}`}
-                        onClick={() => handleServiceClick(service.name)}
+                        className={`p-4 border rounded-lg shadow cursor-pointer hover:bg-indigo-100 hover:text-indigo-900 ${selectedCloud === service ? 'border-4 border-indigo-500' : 'border'}`}
+                        onClick={() => setSelectedCloud(service)}
                     >
                         <div className="flex items-center justify-center space-x-2">
-                            {getIcon(service.name)}
+                            {service.name === 'AWS' ? <FaAws /> : <SiOvh />}
                             <span>{service.name}</span>
                         </div>
                     </div>
                 ))}
             </div>
-            {selectedService && (
-                <h1 className="text-center mt-4">Has elegido {selectedService}</h1>
-            )}
+            {selectedCloud && regions.length > 0 ? (
+                <form className="mt-8 p-8 flex flex-col items-center justify-center text-indigo-900">
+                    <label htmlFor="region" className="p-2 text-center text-indigo-50">Region</label>
+                    <select
+                        className="w-1/2 p-2 border border-gray-300 rounded-md text-center"
+                        onChange={(e) => setSelectedRegion(e.target.value)}
+                        value={selectedRegion}
+                    >
+                        {regions.map(region => (
+                            <option key={region.id} value={region.id}>{region.name}</option>
+                        ))}
+                    </select>
+                    <label htmlFor="distribution" className="mt-4 p-2 text-center text-indigo-50">Linux Distribution</label>
+                    <select className="w-1/2 p-2 border border-gray-300 rounded-md text-center">
+                        {distributions.map(distribution => (
+                            <option key={distribution.id} value={distribution.id}>{distribution.distribution} {distribution.version}</option>
+                        ))}
+                    </select>
+                </form>
+            ) : selectedCloud ? (
+                <p>This cloud provider will be added soon.</p>
+            ) : null}
         </>
     );
 }
