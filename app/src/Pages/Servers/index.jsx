beaa2f524cdc8924ea42b79fbb3c70da26e60121
barckcode <cristian@helmcode.com>
2024-06-10 20:13:07 +0100

    Added styles to modal

--- app/src/Pages/Servers/index.jsx
+++ app/src/Pages/Servers/index.jsx
@@ -238,15 +238,15 @@ export default function CloudServices({ backendUrl, session }) {
                         ))}
                     </select>
                     <div className="mt-8 flex justify-between w-1/2">
-                        <button type="button" className="p-2 w-12 flex items-center justify-center bg-gray-300 rounded-lg"><FaArrowLeft /></button>
-                        <button type="button" className="p-2 w-32 text-center bg-gray-300 rounded-lg" onClick={openModal}>Summary</button>
+                        <button type="button" className="inline-flex justify-center items-center rounded-md border border-transparent bg-rose-500 px-4 py-2 text-sm font-medium text-indigo-50 shadow-sm hover:bg-rose-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-rose-500 focus-visible:ring-offset-2"><FaArrowLeft /></button>
+                        <button type="button" className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-indigo-50 shadow-sm hover:bg-indigo-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-indigo-500 focus-visible:ring-offset-2" onClick={openModal}>Summary</button>
                     </div>
                 </form>
             ) : selectedCloud ? (
                 <p>This cloud provider will be added soon.</p>
             ) : null}
             <Transition show={modalIsOpen} as={Fragment}>
-                <Dialog as="div" className="relative z-10" onClose={closeModal}>
+                <Dialog as="div" className="relative z-10 text-indigo-900" onClose={closeModal}>
                     <Transition.Child
                         as={Fragment}
                         enter="ease-out duration-300"
@@ -256,7 +256,7 @@ export default function CloudServices({ backendUrl, session }) {
                         leaveFrom="opacity-100"
                         leaveTo="opacity-0"
                     >
-                        <div className="fixed inset-0 bg-gray-500 bg-opacity-75" />
+                        <div className="fixed inset-0 bg-indigo-200 bg-opacity-50" />
                     </Transition.Child>
 
                     <div className="fixed inset-0 overflow-y-auto">
@@ -270,32 +270,50 @@ export default function CloudServices({ backendUrl, session }) {
                                 leaveFrom="opacity-100 translate-y-0 sm:scale-100"
                                 leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                             >
-                                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
-                                    <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
+                                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-indigo-50 px-4 pt-5 pb-4 shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+                                    <Dialog.Title as="h3" className="text-xl font-bold p-4 leading-6 text-indigo-900 text-center">
                                         Server Summary
                                     </Dialog.Title>
-                                    <div className="mt-2">
-                                        <p className="text-sm text-gray-500">
-                                            Cloud: {selectedCloud?.name || 'Select a cloud'}
-                                            <br />
-                                            Name: {serverName}
-                                            <br />
-                                            Region: {selectedRegion?.name || 'Select a region'}
-                                            <br />
-                                            Distribution: {selectedDistribution?.distribution} {selectedDistribution?.version}
-                                            <br />
-                                            Instance Type: {selectedInstanceType?.cpu} / {selectedInstanceType?.memory}
-                                            <br />
-                                            Storage: {selectedStorage?.size} GiB
-                                        </p>
+                                    <div className="mt-2 text-lg">
+                                        <div className="flex p-4 items-center text-center border-t-2 border-indigo-100">
+                                            <h4 className="w-1/2 font-medium">Cloud:</h4>
+                                            <p className="w-1/2">{selectedCloud?.name || 'Select a cloud'}</p>
+                                        </div>
+                                        <div className="flex p-4 items-center text-center border-t-2 border-indigo-100">
+                                            <h4 className="w-1/2 font-medium">Name:</h4>
+                                            <p className="w-1/2">{serverName}</p>
+                                        </div>
+                                        <div className="flex p-4 items-center text-center border-t-2 border-indigo-100">
+                                            <h4 className="w-1/2 font-medium">Region:</h4>
+                                            <p className="w-1/2">{selectedRegion?.name || 'Select a region'}</p>
+                                        </div>
+                                        <div className="flex p-4 items-center text-center border-t-2 border-indigo-100">
+                                            <h4 className="w-1/2 font-medium">Distribution:</h4>
+                                            <p className="w-1/2">{selectedDistribution?.distribution} {selectedDistribution?.version}</p>
+                                        </div>
+                                        <div className="flex p-4 items-center text-center border-t-2 border-indigo-100">
+                                            <h4 className="w-1/2 font-medium">Instance Type:</h4>
+                                            <p className="w-1/2">{selectedInstanceType?.cpu} / {selectedInstanceType?.memory}</p>
+                                        </div>
+                                        <div className="flex p-4 items-center text-center border-t-2 border-indigo-100">
+                                            <h4 className="w-1/2 font-medium">Storage:</h4>
+                                            <p className="w-1/2">{selectedStorage?.size} GiB</p>
+                                        </div>
                                     </div>
-                                    <div className="mt-4">
+                                    <div className="w-full mt-4 flex justify-evenly">
                                         <button
                                             type="button"
-                                            className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-indigo-500 focus-visible:ring-offset-2"
+                                            className="inline-flex justify-center rounded-md border border-transparent bg-rose-500 px-4 py-2 text-sm font-medium text-indigo-50 shadow-sm hover:bg-rose-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-rose-500 focus-visible:ring-offset-2"
                                             onClick={closeModal}
                                         >
                                             Close
+                                        </button>
+                                        <button
+                                            type="button"
+                                            className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-indigo-500 focus-visible:ring-offset-2"
+                                            // onClick={}
+                                        >
+                                            Create
                                         </button>
                                     </div>
                                 </Dialog.Panel>
